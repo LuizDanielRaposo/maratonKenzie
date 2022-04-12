4ec531d7a05de660bd67d4719465149f806b8819
LuizDanielRaposo <84152889+LuizDanielRaposo@users.noreply.github.com>
2022-04-12 10:58:55 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,17 @@
+function positions(firstPlace, secondPlace, lastPlace){
+    let podio = [firstPlace, secondPlace,lastPlace];
+    if (secondPlace == "Daniel") {
+        podio = [secondPlace, firstPlace, lastPlace];
+    }else if (lastPlace == "Daniel") {
+        podio = [firstPlace, lastPlace, secondPlace];
+    }else{
+        podio = [firstPlace, secondPlace, lastPlace];
+    }
+    console.log('Primeiro colocado - '+ podio[0]);
+    console.log('Segundo colocado - '+ podio[1]);
+    console.log('Terceiro colocado - '+ podio[2]);
+}
+let resultado = positions('Rafael','Manoel','Daniel');
+let resultado2 = positions('Manoel', 'Daniel', 'Rafael');
+
 
